091747eaf152ea24ac8f6b9ca38ffb308d4498f3
changchengtu <changcheng.tu@gmail.com>
2013-03-02 00:06:08 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Crowdfunding::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => 'nthucrowdfunding.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => "587",

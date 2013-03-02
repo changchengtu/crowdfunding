4c8bb78c84774af7df26d5ae06a3d7edac7b5775
changchengtu <changcheng.tu@gmail.com>
2013-03-02 23:08:48 +0800

    test

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -19,23 +19,23 @@ class ProjectController < ApplicationController
 
   def reflashindex
     page = params[:page]
-    @allproject = Pro.all
+    @allproject = Pro.where(:PmanagerOn=>true)
     if page == '1'
-      @allproject = Pro.where(:Pclassify=>"1")
+      @allproject = Pro.where(:Pclassify=>"1",:PmanagerOn=>true)
     elsif page == '2' 
-      @allproject = Pro.where(:Pclassify=>"2")
+      @allproject = Pro.where(:Pclassify=>"2",:PmanagerOn=>true)
     elsif page == '3'
-      @allproject = Pro.where(:Pclassify=>"3")
+      @allproject = Pro.where(:Pclassify=>"3",:PmanagerOn=>true)
     elsif page == '4'
-      @allproject = Pro.where(:Pclassify=>"4")
+      @allproject = Pro.where(:Pclassify=>"4",:PmanagerOn=>true)
     elsif page == '5'
-      @allproject = Pro.where(:Pclassify=>"5")
+      @allproject = Pro.where(:Pclassify=>"5",:PmanagerOn=>true)
     elsif page == '6'
-      @allproject = Pro.where(:Pclassify=>"6")
+      @allproject = Pro.where(:Pclassify=>"6",:PmanagerOn=>true)
     elsif page == '7'
-      @allproject = Pro.where(:Pclassify=>"7")
+      @allproject = Pro.where(:Pclassify=>"7",:PmanagerOn=>true)
     elsif page == '8'
-      @allproject = Pro.where(:Pclassify=>"8")
+      @allproject = Pro.where(:Pclassify=>"8",:PmanagerOn=>true)
     end
   end
 end

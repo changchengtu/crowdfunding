d87208f9f8131a9c062794bd53de53698c972c87
changchengtu <changcheng.tu@gmail.com>
2013-03-04 15:55:31 +0800

    test

--- app/controllers/startaproject_controller.rb
+++ app/controllers/startaproject_controller.rb
@@ -29,6 +29,7 @@ class StartaprojectController < ApplicationController
     @pro = Pro.find(params[:id])
     if @pro.Puseron = true
       params[:content].start = Time.now
+    end
     @pro.update_attributes!(params[:content])
     redirect_to startaproject_index_path
   end

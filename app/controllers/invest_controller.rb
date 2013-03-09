70672ea2d40910ebc0e21f8ad2d5c791fabf56df
changchengtu <changcheng.tu@gmail.com>
2013-03-09 13:26:41 +0800

    test

--- app/controllers/invest_controller.rb
+++ app/controllers/invest_controller.rb
@@ -2,8 +2,6 @@ class InvestController < ApplicationController
   before_filter :authenticate_user!
   def edit
     @thisp = Pro.find(params[:id])
-    @pro_id = params[:id].to_i
-    @user_id = current_user.id 
   end
   def update
     Investment.create(params[:invest])

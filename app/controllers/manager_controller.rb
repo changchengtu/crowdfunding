9f9a2d38f85744029e6f692b9e3e9e97677d19c8
changchengtu <changcheng.tu@gmail.com>
2013-03-14 22:27:10 +0800

    test

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -30,7 +30,7 @@ class ManagerController < ApplicationController
     @confirm.update_attributes!(params[:confirm])
    
     if @confirm.confirm
-      Pro.create(:name=>params[:confirm][:name], :classify=>@confirm.classify, :goal=>@confirm.goal, :user_id=>@confirm.user_id, :useron=>false, :manageron=>false)
+      Pro.create(:name=>params[:confirm][:name], :classify=>@confirm.classify, :goal=>@confirm.goal, :got=>0, :user_id=>@confirm.user_id, :useron=>false, :manageron=>false)
       Usernotice.sendemail(User.find(@confirm.user_id).email,'project information','your project:'+@confirm.name+' has been confirmed').deliver
     end
     redirect_to manager_index_path

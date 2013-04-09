056a81d17d4b68ca7a87fbd38b836a6bf178f4a5
changchengtu <changcheng.tu@gmail.com>
2013-04-09 17:26:06 +0800

    test

--- app/controllers/invest_controller.rb
+++ app/controllers/invest_controller.rb
@@ -10,7 +10,7 @@ class InvestController < ApplicationController
     @thisp = Pro.find(params[:id])
     sum = @thisp.got.to_i + params[:invest][:howmuch].to_i
     @thisp.update_attributes(:got=>sum)
-    Usernotice.sendmail(@thisp.user.email,'You got support money!','From'＋User.find(params[:invest][:user_id]).email+' support you'＋params[:invest][:howmuch]+' NTD')
+    Usernotice.sendemail(@thisp.user.email,'You got support money!','From'＋User.find(params[:invest][:user_id]).email+' support you'＋params[:invest][:howmuch]+' NTD')
     redirect_to root_path
   end
 end
